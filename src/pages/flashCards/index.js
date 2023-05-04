645071d99e501ed9c378f8c30cb312967f8fc70c
IgorWilbert <igor.wilbert@gmail.com>
2023-05-04 11:08:32 -0300

    Isolate FlashCardsData in data folder

--- src/pages/flashCards/index.js
+++ src/pages/flashCards/index.js
@@ -1,43 +1,11 @@
 import { useState } from "react";
 
+import FlashCardsData from "../../data/flashCards";
+
 import FlashCard from "../../components/flashCard";
 import Title from "../../components/title";
 
 import "./styles.css";
-
-const FlashCardsData = [
-  {
-    imageUrl: "https://source.unsplash.com/random/300×300",
-    question: "Qual é o nome da taxa que mede o valor da inflação no Brasil?",
-    answer:
-      "O nome da taxa que mede a inflação no Brasil é Índice Nacional de Preços ao Consumidor Amplo (IPCA)",
-  },
-  {
-    imageUrl: "https://source.unsplash.com/random/300×300",
-    question: "texto da pergunta 2",
-    answer: "texto da resposta 2",
-  },
-  {
-    imageUrl: "https://source.unsplash.com/random/300×300",
-    question: "texto da pergunta 3",
-    answer: "texto da resposta 3",
-  },
-  {
-    imageUrl: "https://source.unsplash.com/random/300×300",
-    question: "texto da pergunta 4",
-    answer: "texto da resposta 4",
-  },
-  {
-    imageUrl: "https://source.unsplash.com/random/300×300",
-    question: "texto da pergunta 5",
-    answer: "texto da resposta 5",
-  },
-  {
-    imageUrl: "https://source.unsplash.com/random/300×300",
-    question: "texto da pergunta 6",
-    answer: "texto da resposta 6",
-  },
-];
 
 const FlashCards = () => {
   const [cardIndex, setCardIndex] = useState(0);

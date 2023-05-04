86f6e346467943d0298f3fe9a3e3703eab07ba53
IgorWilbert <igor.wilbert@gmail.com>
2023-05-04 10:59:26 -0300

    Update quiz questions and add new ones

--- src/data/quiz.js
+++ src/data/quiz.js
@@ -1,82 +1,351 @@
 const QuizData = [
   {
-    heading: "Qual é o nome da taxa que mede o valor da inflação no Brasil?",
+    heading: "O que significa a sigla CDI?",
     value: 20,
     selectedOption: -1,
     options: [
       {
-        text: "IPCA",
+        text: "Certificado de Depósito Interbancário",
         correct: true,
       },
       {
-        text: "SELIC",
+        text: "Companhia de Desenvolvimento Industrial",
         correct: false,
       },
       {
-        text: "CDI",
+        text: "Cota Diária de Investimento",
         correct: false,
       },
       {
-        text: "Bovespa",
+        text: "Coeficiente de Desempenho do Investimento",
+        correct: false,
+      },
+    ],
+  },
+  {
+    heading: "Qual é a principal função do CDI?",
+    value: 30,
+    selectedOption: -1,
+    options: [
+      {
+        text: "Servir como referência para as taxas de juros nos investimentos",
+        correct: true,
+      },
+      {
+        text: "Medir a inflação no Brasil",
         correct: false,
       },
       {
-        text: "IPVA",
+        text: "Regular o mercado de ações",
+        correct: false,
+      },
+      {
+        text: "Controlar a oferta de crédito no mercado",
+        correct: false,
+      },
+    ],
+  },
+  {
+    heading: "O que significa a sigla IPCA?",
+    value: 40,
+    selectedOption: -1,
+    options: [
+      {
+        text: "Índice de Preços ao Consumidor Amplo",
+        correct: true,
+      },
+      {
+        text: "Índice de Preços Comerciais Agregados",
+        correct: false,
+      },
+      {
+        text: "Índice de Preços das Commodities Agrícolas",
+        correct: false,
+      },
+      {
+        text: "Índice de Preços das Companhias Aéreas",
+        correct: false,
+      },
+    ],
+  },
+  {
+    heading: "Qual é a função do IPCA?",
+    value: 40,
+    selectedOption: -1,
+    options: [
+      {
+        text: "Medir a variação de preços de bens e serviços consumidos pelas famílias brasileiras",
+        correct: true,
+      },
+      {
+        text: "Regular o mercado de câmbio",
+        correct: false,
+      },
+      {
+        text: "Controlar a inflação nos investimentos",
+        correct: false,
+      },
+      {
+        text: "Servir como referência para a taxa básica de juros",
+        correct: false,
+      },
+    ],
+  },
+  {
+    heading: "O que é a B3?",
+    value: 40,
+    selectedOption: -1,
+    options: [
+      {
+        text: "Bolsa de Valores de São Paulo",
+        correct: true,
+      },
+      {
+        text: "Banco Central do Brasil",
+        correct: false,
+      },
+      {
+        text: "Bolsa de Valores do Rio de Janeiro",
+        correct: false,
+      },
+      {
+        text: "Banco Nacional de Desenvolvimento Econômico e Social",
+        correct: false,
+      },
+    ],
+  },
+  {
+    heading: "Qual é a principal função da B3?",
+    value: 40,
+    selectedOption: -1,
+    options: [
+      {
+        text: "Negociar ações, títulos e outros ativos financeiros",
+        correct: true,
+      },
+      {
+        text: "Regulamentar o mercado financeiro brasileiro",
+        correct: false,
+      },
+      {
+        text: "Fornecer informações sobre as empresas que estão listadas na bolsa de valores",
+        correct: false,
+      },
+      {
+        text: "Controlar a oferta de crédito no mercado",
+        correct: false,
+      },
+    ],
+  },
+  {
+    heading: "O que é um fundo de investimento?",
+    value: 40,
+    selectedOption: -1,
+    options: [
+      {
+        text: "Um conjunto de recursos de diversos investidores que são aplicados em diversos ativos financeiros",
+        correct: true,
+      },
+      {
+        text: "Uma empresa que vende ações para o público em geral",
+        correct: false,
+      },
+      {
+        text: "Uma instituição financeira que empresta dinheiro para outras empresas",
+        correct: false,
+      },
+      {
+        text: "Uma instituição que controla a oferta de crédito no mercado",
         correct: false,
       },
     ],
   },
   {
     heading:
-      "Qual é a estratégia recomendada a investidores conservadores quando o valor dos ativos no mercado de ações cai?",
-    value: 30,
+      "Qual das seguintes opções oferece potencialmente um retorno maior em longo prazo?",
+    value: 40,
     selectedOption: -1,
     options: [
       {
-        text: "Vender parte dos ativos",
+        text: "Renda variável",
         correct: true,
       },
       {
-        text: "Vender todos os ativos",
+        text: "Renda fixa",
         correct: false,
       },
       {
-        text: "Comprar uma pequena quantidade dos mesmos ativos",
+        text: "Investimento imobiliário",
         correct: false,
       },
       {
-        text: "Comprar uma grande quantidade dos mesmos ativos",
-        correct: false,
-      },
-      {
-        text: "Comprar uma pequena quantidade de outros ativos",
+        text: "Investimento em criptomoedas",
         correct: false,
       },
     ],
   },
   {
-    heading: "O que significa a sigla FGC?",
+    heading: "Qual dos seguintes investimentos é considerado de renda fixa?",
     value: 40,
     selectedOption: -1,
     options: [
       {
-        text: "Fundo Garantidor de Crédito",
+        text: "Debêntures",
         correct: true,
       },
       {
-        text: "Fundos Globais Conservadores",
+        text: "Ações de uma empresa",
         correct: false,
       },
       {
-        text: "Financiamento Governamental Comercial",
+        text: "Fundos imobiliários",
         correct: false,
       },
       {
-        text: "Fiscalização Governamental Conjunta",
+        text: "Commodities",
+        correct: false,
+      },
+    ],
+  },
+  {
+    heading:
+      "Qual das seguintes opções geralmente tem maior risco associado a ela?",
+    value: 40,
+    selectedOption: -1,
+    options: [
+      {
+        text: "Investimento em ações de uma empresa estabelecida",
+        correct: true,
+      },
+      {
+        text: "Investimento em um fundo imobiliário",
         correct: false,
       },
       {
-        text: "Fundo Governamental de Crédito",
+        text: "Investimento em títulos do Tesouro Nacional",
+        correct: false,
+      },
+      {
+        text: "Investimento em um fundo de renda fixa",
+        correct: false,
+      },
+    ],
+  },
+  {
+    heading:
+      "Qual dos seguintes tipos de investimentos oferece maior liquidez?",
+    value: 40,
+    selectedOption: -1,
+    options: [
+      {
+        text: "Investimentos em renda fixa",
+        correct: true,
+      },
+      {
+        text: "Investimento em imóveis",
+        correct: false,
+      },
+      {
+        text: "Investimentos em ações",
+        correct: false,
+      },
+      {
+        text: "Investimentos em commodities",
+        correct: false,
+      },
+    ],
+  },
+  {
+    heading: "O que é diversificação de investimentos?",
+    value: 40,
+    selectedOption: -1,
+    options: [
+      {
+        text: "Investir em várias classes de ativos financeiros diferentes",
+        correct: true,
+      },
+      {
+        text: "Investir em apenas um tipo de ativo financeiro",
+        correct: false,
+      },
+      {
+        text: "Investir em apenas um setor da economia",
+        correct: false,
+      },
+      {
+        text: "Investir em apenas uma empresa",
+        correct: false,
+      },
+    ],
+  },
+  {
+    heading:
+      "Qual dos seguintes investimentos é considerado de renda variável?",
+    value: 40,
+    selectedOption: -1,
+    options: [
+      {
+        text: "Fundos de investimento em ações",
+        correct: true,
+      },
+      {
+        text: "Títulos do Tesouro Nacional",
+        correct: false,
+      },
+      {
+        text: "Debêntures",
+        correct: false,
+      },
+      {
+        text: "Certificados de Depósito Bancário (CDBs)",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    heading: "O que é liquidez?",
+    value: 40,
+    selectedOption: -1,
+    options: [
+      {
+        text: "A capacidade de um ativo financeiro ser convertido em dinheiro sem perda significativa de valor",
+        correct: true,
+      },
+      {
+        text: "A capacidade de um ativo financeiro gerar retornos significativos em curto prazo",
+        correct: false,
+      },
+      {
+        text: "A capacidade de um ativo financeiro ser negociado com outros investidores",
+        correct: false,
+      },
+      {
+        text: "A capacidade de um ativo financeiro oferecer proteção contra inflação",
+        correct: false,
+      },
+    ],
+  },
+  {
+    heading:
+      "Qual dos seguintes investimentos oferece maior segurança em relação ao risco de perda de capital?",
+    value: 40,
+    selectedOption: -1,
+    options: [
+      {
+        text: "Investimento em títulos do Tesouro Nacional",
+        correct: true,
+      },
+      {
+        text: "Investimento em ações",
+        correct: false,
+      },
+      {
+        text: "Investimento em commodities",
+        correct: false,
+      },
+      {
+        text: "Investimento em criptomoedas",
         correct: false,
       },
     ],

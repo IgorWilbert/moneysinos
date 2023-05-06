a4772bc290110e4f660f70bab0384f654a99b901
IgorWilbert <igor.wilbert@gmail.com>
2023-05-06 15:07:57 -0300

    Add images for flashcarcards

--- src/data/flashCards.js
+++ src/data/flashCards.js
@@ -1,36 +1,36 @@
 const FlashCardsData = [
   {
-    imageUrl: "https://source.unsplash.com/random/300×300",
+    imageUrl: `${process.env.PUBLIC_URL}/flashcards/carteira.jpg`,
     question: "O que significa a sigla CDI e qual é a sua principal função?",
     answer:
       "O CDI significa Certificado de Depósito Interbancário. É uma taxa de juros utilizada nos empréstimos entre os bancos. Ela é usada como principal índice dos investimentos de renda fixa (aqueles que possuem uma rentabilidade previsível). Assim, muitos investimentos em renda fixa possuem sua rentabilidade atrelada ao CDI.",
   },
   {
-    imageUrl: "https://source.unsplash.com/random/300×300",
+    imageUrl: `${process.env.PUBLIC_URL}/flashcards/cofrinho.jpeg`,
     question: "O que significa a sigla IPCA e qual é a sua principal função?",
     answer:
       "O IPCA significa Índice de Preços ao Consumidor Amplo. É uma taxa cuja principal função é medir a variação de preços de bens e serviços consumidos pelas famílias brasileiras. Criado em 1979, o indicador mede a variação dos preços de um conjunto de produtos e serviços vendidos no varejo e consumidos pelos brasileiros.",
   },
   {
-    imageUrl: "https://source.unsplash.com/random/300×300",
+    imageUrl: `${process.env.PUBLIC_URL}/flashcards/financas.png`,
     question: "O que significa a sigla SELIC e qual é a sua principal função?",
     answer:
       "SELIC é a sigla para Sistema Especial de Liquidação e de Custódia. Ela é a taxa básica de juros da economia. É o principal instrumento de política monetária utilizado pelo Banco Central (BC) para controlar a inflação. Ela influencia todas as taxas de juros do país, como as taxas de juros dos empréstimos, dos financiamentos e das aplicações financeiras.",
   },
   {
-    imageUrl: "https://source.unsplash.com/random/300×300",
+    imageUrl: `${process.env.PUBLIC_URL}/flashcards/investidor.png`,
     question: "O que significa a sigla B3 e qual é a sua principal função?",
     answer:
       "B3, também conhecida como Bolsa de Valores de São Paulo, é a sigla para Brasil, Bolsa e Balcão. A B3 é responsável por todas as atividades necessárias para que investidores possam negociar ações, títulos e outros ativos financeiros",
   },
   {
-    imageUrl: "https://source.unsplash.com/random/300×300",
+    imageUrl: `${process.env.PUBLIC_URL}/flashcards/linear.jpg`,
     question: "O que é um fundo de investimento?",
     answer:
       "Um fundo de investimento é um conjunto de recursos de diversos investidores que são aplicados em diversos ativos financeiros",
   },
   {
-    imageUrl: "https://source.unsplash.com/random/300×300",
+    imageUrl: `${process.env.PUBLIC_URL}/flashcards/oscilatoria.jpg`,
     question:
       "Qual é a classe de investimentos que oferece potencialmente um retorno maior em longo prazo?",
     answer:
@@ -38,45 +38,45 @@ const FlashCardsData = [
   },
 
   {
-    imageUrl: "https://source.unsplash.com/random/300×300",
+    imageUrl: `${process.env.PUBLIC_URL}/flashcards/real.jpg`,
     question: "Quais são os principais investimentos do tipo renda fixa?",
     answer:
       "Os principais investimentos do tipo renda fixa são: Tesouro Direto, CDB (Certificado de Depósito Bancário), Debêntures, LCI (Letra de Crédito Imobiliário), LCA (Letra de Crédito do Agronegócio), CRI (Certificado de Recebíveis Imobiliários) e CRA (Certificado de Recebimento do Agronegócio)",
   },
   {
-    imageUrl: "https://source.unsplash.com/random/300×300",
+    imageUrl: `${process.env.PUBLIC_URL}/flashcards/imovel.jpg`,
     question: "Qual tipo de investimento têm maior risco associado?",
     answer:
       "O investimento em ações de uma empresa estabelecida geralmente têm maior risco associado. Isso porque a natureza das operações da empresa em um mercado de livre competição faz com que o valor da empresa oscile ao longo do tempo, às vezes em alta, às vezes em baixa. Além disso, nem todas as empresas permitem que investidores participem de decisões operacionais. Assim, o risco está associado à grande dificuldade de prever o retorno do investimento em ações de empresas.",
   },
 
   {
-    imageUrl: "https://source.unsplash.com/random/300×300",
+    imageUrl: `${process.env.PUBLIC_URL}/flashcards/viajar.jpg`,
     question: "Qual tipo de investimento têm maior liquidez?",
     answer:
       'O investimento em renda fixa têm maior liquidez. Isso porque o retorno do investimento em renda fixa é altamente previsível, ou seja, não há desvantagem em sacar o dinheiro no "momento errado" assim como ocorre com investimentos em renda variável. Além disso, existem alguns investimentos de renda fixa, como CDBs de Disponibilidade Diária, que permitem ao investidor sacar parte ou todo o capital investido em qualquer dia útil.',
   },
 
   {
-    imageUrl: "https://source.unsplash.com/random/300×300",
+    imageUrl: `${process.env.PUBLIC_URL}/flashcards/carro.jpg`,
     question: "O que é diversificação de investimentos?",
     answer:
       "Diversificação de investimentos consiste em investir em várias classes de ativos financeiros diferentes. A vantagem é que caso uma ou mais classes de ativos esteja em baixa, a alta dos demais ativos compensa as perdas. A desvantagem é que os retornos do investimento serão menores, pois as chances de todas as classes de ativos estarem em alta ao mesmo tempo são baixas.",
   },
   {
-    imageUrl: "https://source.unsplash.com/random/300×300",
+    imageUrl: `${process.env.PUBLIC_URL}/flashcards/faculdade.jpeg`,
     question: "Quais são os principais investimentos do tipo renda variável?",
     answer:
       "Os principais investimentos do tipo renda variável são: Ações de Empresas, FIIs (Fundos de Investimentos Imobiliários), ETFs (Fundos de Índices), Fundos de Ações e Fundos Multimercados.",
   },
   {
-    imageUrl: "https://source.unsplash.com/random/300×300",
+    imageUrl: `${process.env.PUBLIC_URL}/flashcards/cruzeiro.jpg`,
     question: "O que é liquidez de um investimento?",
     answer:
       "Liquidez é a capacidade de um ativo financeiro ser convertido em dinheiro sem perda significativa de valor. A liquidez é uma característica importante para investidores, pois mede a facilidade de entrar e sair de uma posição de investimento. Alguns investimentos têm liquidez extremamente alta, como CDBs de Disponibilidade Diária. Outros investimentos têm liquidez extremamente baixa, como investimentos em Imóveis.",
   },
   {
-    imageUrl: "https://source.unsplash.com/random/300×300",
+    imageUrl: `${process.env.PUBLIC_URL}/flashcards/chovendo.jpg`,
     question:
       "Qual investimento oferece maior segurança em relação ao risco de perda de capital?",
     answer:

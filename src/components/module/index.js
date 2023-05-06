4f5bd2ed33a9bc95daa7b104b85683cb816adbda
IgorWilbert <igor.wilbert@gmail.com>
2023-05-06 14:01:05 -0300

    Update images in home and style of module component

--- src/components/module/index.js
+++ src/components/module/index.js
@@ -6,16 +6,17 @@ const Module = ({ title, description, imageUrl, linkUrl }) => (
   <a href={linkUrl} style={{ textDecoration: "none" }}>
     <div
       className="module"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
+      // style={{
+      //   backgroundImage: `url(${imageUrl})`,
+      // }}
     >
+      <img className="module-image" src={imageUrl}></img>
       <div className="module-text">
         <h2 className="module-title">{title}</h2>
         <p className="module-description">{description}</p>
-      </div>
-      <div className="module-button">
-        <ArrowButton />
+        <div className="module-button">
+          <ArrowButton />
+        </div>
       </div>
     </div>
   </a>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,19 +5,19 @@ const Home = () => (
   <div className="home">
     <Module
       title="FlashCards"
-      imageUrl={"https://source.unsplash.com/random/300×300"}
+      imageUrl={`${process.env.PUBLIC_URL}/home/flashcards.jpg`}
       description="Amplie seu conhecimento sobre investimentos"
       linkUrl="#/flashcards"
     />
     <Module
       title="Quiz"
-      imageUrl={"https://source.unsplash.com/random/300×300"}
+      imageUrl={`${process.env.PUBLIC_URL}/home/quiz.jpg`}
       description="Teste seu conhecimento sobre investimentos"
       linkUrl="#/quiz"
     />
     <Module
       title="Mercado Atual"
-      imageUrl={"https://source.unsplash.com/random/300×300"}
+      imageUrl={`${process.env.PUBLIC_URL}/home/mercado-atual.jpg`}
       description="Principais índices e tendências para ajudar você a investir"
       linkUrl="#/mercado-atual"
     />

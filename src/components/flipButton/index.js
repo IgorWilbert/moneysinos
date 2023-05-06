2d57a929467ef5da9c6143386864229149132650
IgorWilbert <igor.wilbert@gmail.com>
2023-05-06 20:18:02 -0300

    Update icon in FlipButton component

--- src/components/flipButton/index.js
+++ src/components/flipButton/index.js
@@ -1,12 +1,10 @@
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import FlipIcon from "@mui/icons-material/Flip";
 
 import "./styles.css";
 
 const FlipButton = ({ onClick }) => (
   <div className="flip-button" onClick={onClick}>
-    <ArrowUpwardIcon />
-    <ArrowDownwardIcon />
+    <FlipIcon />
   </div>
 );
 
